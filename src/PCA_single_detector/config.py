7591069239e3d4a8dd2a4e04a6054795bcfe35d4
Anna C. <alynnechristopherson@gmail.com>
2025-09-24 15:47:23 -0600

    "Created all scripts for the project. Added comments to each outlining the goals the script should do and copied the current code that proforms most of the expectation which is from the old code and has not been modified to work yet."

--- src/PCA_single_detector/config.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from pathlib import Path
-
-# Configuration Constants
-PROCESSED_DATA_PATH = Path('/home/anna/Machine_Learning_Xmax/data/processed_tree_sample.parquet')
-OUTPUT_DATA_PATH = Path('/home/anna/Machine_Learning_Xmax/data/PCA_single_detector_25PC.parquet')
-N_COMPONENTS_PCA = 128
-WAVEFORM_LENGTH = 128
